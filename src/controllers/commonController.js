5c3dbbbc07d1811845738ebf22563e23dbeed897
Adarsh V S <adarsh@zdistancelab.com>
2023-05-30 10:23:51 +0530

    added fetch all wallet addresses

--- src/controllers/commonController.js
+++ src/controllers/commonController.js
@@ -1,6 +1,29 @@
-const commonViewPatientDetails = async(req,res)=>{
+const {
+    fetchWalletAddressesService,
+} = require("../services/blockchain/blockchainService");
 
-}
-module.exports={
-    commonViewPatientDetails
-}
+const commonViewPatientDetails = async (req, res) => {};
+
+const fetchAllWalletAddressesController = async (req, res) => {
+    console.log("hit");
+    try {
+        fetchWalletAddressesService().then((response) => {
+            if (response.status == "success") {
+                res.status(200).send(response);
+            } else {
+                res.status(400).send(response);
+            }
+        });
+    } catch (error) {
+        const response = {
+            status: "failed",
+            message: error.message,
+        };
+        res.status(400).send(response);
+    }
+};
+
+module.exports = {
+    commonViewPatientDetails,
+    fetchAllWalletAddressesController,
+};
--- src/routes/commonRoutes.js
+++ src/routes/commonRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const commonControllers = require("../controllers/commonController")
+const commonControllers = require("../controllers/commonController");
 
-router.get("/view-patient-details",commonControllers.commonViewPatientDetails);
-
+router.get("/view-patient-details", commonControllers.commonViewPatientDetails);
+router.get("/fetch-wallet-addresses", commonControllers.fetchAllWalletAddressesController);
 
 module.exports = router;
